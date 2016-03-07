28dd556c6d6f45235fdc60da67da24b4d6433cf7
Manuel <manubp9@gmail.com>
2016-03-07 12:36:13 +0100

    creada clase mihashmap y añadido atributos y constructor

---
+++ MiHashMap.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+/**
+ * Write a description of class MiHashMap here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class MiHashMap
+{
+    // instance variables - replace the example below with your own
+    private String[] letras;
+    //
+    private int[] numeros;
+
+    /**
+     * Constructor for objects of class MiHashMap
+     */
+    public MiHashMap()
+    {
+        letras = new String[0];
+        numeros = new int[0];
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
